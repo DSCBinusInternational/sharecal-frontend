6f461bf22829eeb67af8a650cfad6cf5ad8f4bd5
Jason Christian H <nakamarujc@gmail.com>
2022-04-30 14:51:28 +0700

    Added calendar:
    - Created logic in calendar
    - Calendar layout

--- src/pages/Calendar/CalendarComponent.jsx
+++ src/pages/Calendar/CalendarComponent.jsx
@@ -1,6 +1,45 @@
+import "./CalendarComponent.css";
+import dayjs from "dayjs";
+
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 function CalendarComponent() {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const date = dayjs();
+  const startingDayPos = date.date(1).day();
+  const daysInPrevMonth = date.month(date.month() - 1).daysInMonth();
+  const daysInCurMonth = date.daysInMonth();
+
+  const $prevMonth = [];
+  for (let i = 0; i < startingDayPos; i++) {
+    $prevMonth.push(<div className="cell dates prev-month">
+      {daysInPrevMonth - startingDayPos + i + 1}
+    </div>)
+  }
+
+  const $curMonth = [];
+  for (let i = 0; i < daysInCurMonth; i++) {
+    $curMonth.push(<div className="cell dates">
+      {i + 1}
+    </div>)
+  }
+
   return <div className="calendar">
-    Test
+    <h1>{month[date.month()]} {date.year()}</h1>
+    <div className="main-calendar">
+      {
+        days.map((day) => (
+          <div className="cell">{day}</div>
+        ))
+      }
+      {
+        $prevMonth
+      }
+      {
+        $curMonth
+      }
+    </div>
   </div>
 }
 
